1ef376ad11f6d9515dfead6bdd6d8c55c3a62aff
Muthu404200 <muthu225420@gmail.com>
2024-11-16 07:41:43 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div className="header p-10 bg-blue-600 text-center">
+      <h1 className='text-4xl text-white font-bold'>Welcome to java</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
