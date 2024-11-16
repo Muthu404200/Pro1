2dd30afe53087fe24a92aefc714c25e529c6ce1e
Muthu404200 <muthu225420@gmail.com>
2024-11-16 08:03:13 -0800

    Updated components

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function App() {
   return (
     <>
-    <div className="header p-10 bg-blue-600 text-center">
-      <h1 className='text-4xl text-white font-bold'>Welcome to java</h1>
+    <div className="header p-10 text-white bg-blue-600 text-center">
+      <h1 className='text-4xl  font-bold '>Welcome to java</h1>
+      <p className='py-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam repellendus sequi sit nam consequatur, fugiat velit voluptate error provident, rem odio voluptatem delectus! Veritatis voluptatibus rem sapiente earum est dolor.</p>
     </div>
     </>
   )
